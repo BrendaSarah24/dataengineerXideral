f98112bd3754d7f96ee172c36c2818895a394b91
Brenda Sarahí Rosas Morán <91776782+BrendaSarah24@users.noreply.github.com>
2025-01-14 12:43:14 -0600

    Add files via upload

--- proptt2.py
+++ proptt2.py
@@ -1,12 +1,18 @@
 import numpy as np
 arr = np.array([1, 2, 3, 4])
-print(arr)
+#print(arr)
  
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 suma = a + b       
-producto = a * 2   
+producto = a * 2 
+cruzmatriz=a*b  
 
-print(suma)
-print(producto)
- 
+#print(suma)
+#print(producto)
+print(cruzmatriz)
+
+matriz = np. array ([[1, 2], [3, 4]]) 
+
+transpuesta = matriz.T  # [[1, 3], [2, 4]] 
+print(transpuesta)
