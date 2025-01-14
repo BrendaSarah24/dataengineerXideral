3e955b0e2f4e7714225c3d3aad63fab487ccb3b0
Brenda Sarahí Rosas Morán <91776782+BrendaSarah24@users.noreply.github.com>
2025-01-14 12:32:23 -0600

    Add files via upload

---
+++ proptt2.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr)
+ 
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+suma = a + b       
+producto = a * 2   
+
+print(suma)
+print(producto)
+ 
